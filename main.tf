83ac2909e07fee8748acfc134dcc3c161084d053
Robin Norwood <rnorwood@hashicorp.com>
2020-12-17 16:53:40 -0600

    Initial commit

---
+++ main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.19.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.64.0"
+
+  cidr = var.vpc_cidr_block
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e"]
+  private_subnets = slice(var.private_subnet_cidr_blocks, 0, 2)
+  public_subnets  = slice(var.public_subnet_cidr_blocks, 0, 2)
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = false
+}
+
+module "app_security_group" {
+  source  = "terraform-aws-modules/security-group/aws//modules/web"
+  version = "3.17.0"
+
+  name        = "web-server-sg"
+  description = "Security group for web-servers with HTTP ports open within VPC"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks = module.vpc.public_subnets_cidr_blocks
+}
+
+module "lb_security_group" {
+  source  = "terraform-aws-modules/security-group/aws//modules/web"
+  version = "3.17.0"
+
+  name        = "lb-sg-project-alpha-dev"
+  description = "Security group for load balancer with HTTP ports open within VPC"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+}
---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "lb_security_group_ids" {
+  description = "Security group IDs for load balancer"
+  value       = [module.lb_security_group.this_security_group_id]
+}
+
+output "app_security_group_ids" {
+  description = "Security group IDs for application servers"
+  value       = [module.app_security_group.this_security_group_id]
+}
+
+output "public_subnet_ids" {
+  description = "Public subnet IDs"
+  value       = module.vpc.public_subnets
+}
+
+output "private_subnet_ids" {
+  description = "Private subnet IDs"
+  value       = module.vpc.private_subnets
+}
