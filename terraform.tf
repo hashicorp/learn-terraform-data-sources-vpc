1d05ba3ce7213a0f2da4dbd1ad08222fa6b6ae4c
Robin Norwood <rnorwood@hashicorp.com>
2022-06-07 16:44:17 -0500

    Put cloud block first.

--- terraform.tf
+++ terraform.tf
@@ -1,14 +1,14 @@
 terraform {
+  cloud {
+    workspaces {
+      name = "learn-terraform-data-sources-vpc"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.17.1"
-    }
-  }
-
-  cloud {
-    workspaces {
-      name = "learn-terraform-data-sources-vpc"
     }
   }
 
